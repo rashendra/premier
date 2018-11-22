93968d4071c40d7ef943447dd110d19a74392905
Rush <rashendra@gmail.com>
2018-11-22 19:43:37 +1100

    SV-001 Modify Test cases and restrict access modifiers.

--- src/test/java/com/example/premier/service/GameExecuteServiceTest.java
+++ src/test/java/com/example/premier/service/GameExecuteServiceTest.java
@@ -7,38 +7,25 @@ import org.junit.Test;
 public class GameExecuteServiceTest {
 
 	@Test
-	public void shouldReturnIndexOffsetWhenKIsLessThanN() {
-		//Given
-		ChildCircle childCircle = new ChildCircle(9);
-		int k = 7;
-		//when
-		int indexOffset = GameExecuteService.getIndexOffset(k, childCircle);
-		System.out.println(indexOffset);
-		//Then
-		Assertions.assertThat(indexOffset == 7).isTrue();
-	}
-
-	@Test
-	public void shouldReturnIndexOffsetWhenKIsGreaterThanN() {
-		//Given
-		ChildCircle childCircle = new ChildCircle(9);
-		int k = 11;
-		//when
-		int indexOffset = GameExecuteService.getIndexOffset(k, childCircle);
-		//Then
-		Assertions.assertThat(indexOffset == 2).isTrue();
-	}
-
-	@Test
-	public void shouldReturnTheWinnerForTheGivenK() {
+	public void shouldReturnTheWinnerWhenKIsGreaterThanN() {
 		//Given
 		int k = 9;
 		int n = 7;
 		//when
 		Child winner = GameExecuteService.executeGame(k, n);
-		System.out.println(winner.getId());
 		//Then
 		Assertions.assertThat(winner.getId() == 2).isTrue();
+	}
+
+	@Test
+	public void shouldReturnTheWinnerWhenKIsLessThanN() {
+		//Given
+		int k = 7;
+		int n = 11;
+		//when
+		Child winner = GameExecuteService.executeGame(k, n);
+		//Then
+		Assertions.assertThat(winner.getId() == 1).isTrue();
 	}
 
 
